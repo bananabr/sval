5b10ee0a122119fc530fc1ae904a58b43680b162
Daniel Santos <ddossantos@microsoft.com>
2025-03-06 16:05:53 -0600

    improve test coverage

--- tests/declaration.test.ts
+++ tests/declaration.test.ts
@@ -225,7 +225,7 @@ describe('testing src/index.ts', () => {
         exports.cb = cb
         exports.cc = cc
       `)
-    } catch(err) {
+    } catch (err) {
       expect(err).toBeInstanceOf(SyntaxError)
     }
   })
@@ -337,4 +337,213 @@ describe('testing src/index.ts', () => {
     expect(interpreter.exports.f).toBe(4)
     expect(interpreter.exports.y()).toBe(5)
   })
+
+  it('should support class declarations', () => {
+    const interpreter = new Sval()
+
+    const code = `
+      class A {
+      constructor() {
+        this.value = 1
+      }
+
+      method() {
+        return this.value
+      }
+
+      static staticMethod() {
+        return 'static'
+      }
+
+      get prop() {
+        return this.value + 1
+      }
+
+      set prop(val) {
+        this.value = val - 1
+      }
+      }
+
+      class B extends A {
+      constructor() {
+        super()
+        this.value = 2
+      }
+
+      method() {
+        return this.value
+      }
+      }
+
+      const a = new A()
+      const b = new B()
+
+      exports.aValue = a.value
+      exports.aMethod = a.method()
+      exports.aStaticMethod = A.staticMethod()
+      exports.aProp = a.prop
+      a.prop = 5
+      exports.aPropAfterSet = a.prop
+      exports.bValue = b.value
+      exports.bMethod = b.method()
+      exports.bStaticMethod = B.staticMethod()
+    `
+    interpreter.run(`!async function(){${code}}()`) // also test for generator env
+    interpreter.run(code)
+
+    expect(interpreter.exports.aValue).toBe(1)
+    expect(interpreter.exports.aMethod).toBe(1)
+    expect(interpreter.exports.aStaticMethod).toBe('static')
+    expect(interpreter.exports.bValue).toBe(2)
+    expect(interpreter.exports.bMethod).toBe(2)
+    expect(interpreter.exports.bStaticMethod).toBe('static')
+  })
+
+  it('should support static initialization blocks', () => {
+    const interpreter = new Sval()
+
+    const code = `
+      class A {
+        static {
+          this.staticValue = 1
+        }
+
+        static getStaticValue() {
+          return this.staticValue
+        }
+      }
+
+      class B extends A {
+        static {
+          this.staticValue = 2
+        }
+      }
+
+      exports.aStaticValue = A.getStaticValue()
+      exports.bStaticValue = B.getStaticValue()
+    `
+    interpreter.run(`!async function(){${code}}()`) // also test for generator env
+    interpreter.run(code)
+
+    expect(interpreter.exports.aStaticValue).toBe(1)
+    expect(interpreter.exports.bStaticValue).toBe(2)
+  })
+
+  it('should support default export of expression', () => {
+    const interpreter = new Sval({ sourceType: 'module' })
+
+    interpreter.run(`
+      const a = 1
+      export default a
+    `)
+
+    expect(interpreter.exports.default).toBe(1)
+  })
+
+  it('should support default export of named function', () => {
+    const interpreter = new Sval({ sourceType: 'module' })
+
+    interpreter.run(`
+      export default function foo() {
+        return 'foo'
+      }
+    `)
+
+    expect(interpreter.exports.default()).toBe('foo')
+  })
+
+  it('should support default export of named class', () => {
+    const interpreter = new Sval({ sourceType: 'module' })
+
+    interpreter.run(`
+      export default class Foo {
+        constructor() {
+          this.value = 'foo'
+        }
+      }
+    `)
+
+    const Foo = interpreter.exports.default
+    const instance = new Foo()
+    expect(instance.value).toBe('foo')
+  })
+
+  it('should support default export of named generator function', () => {
+    const interpreter = new Sval({ sourceType: 'module' })
+
+    interpreter.run(`
+      export default function* foo() {
+        yield 'foo'
+      }
+    `)
+
+    const generator = interpreter.exports.default()
+    expect(generator.next().value).toBe('foo')
+  })
+
+  it('should support private methods and properties', () => {
+    const interpreter = new Sval()
+
+    interpreter.run(`
+      class A {
+        #privateValue = 1
+
+        #privateMethod() {
+          return this.#privateValue
+        }
+
+        getPrivateValue() {
+          return this.#privateMethod()
+        }
+      }
+
+      const a = new A()
+
+      exports.aPrivateValue = a.getPrivateValue()
+    `)
+
+    expect(interpreter.exports.aPrivateValue).toBe(1)
+  })
+  // TODO: support anonymous function/class/generator function
+  // it('should support default export of anonymous function', () => {
+  //   const interpreter = new Sval({ sourceType: 'module' })
+
+  //   interpreter.run(`
+  //     export default function() {
+  //       return 'foo'
+  //     }
+  //   `)
+
+  //   expect(interpreter.exports.default()).toBe('foo')
+  // })
+
+  // it('should support default export of anonymous class', () => {
+  //   const interpreter = new Sval({ sourceType: 'module' })
+
+  //   interpreter.run(`
+  //     export default class {
+  //       constructor() {
+  //         this.value = 'foo'
+  //       }
+  //     }
+  //   `)
+
+  //   const Foo = interpreter.exports.default
+  //   const instance = new Foo()
+  //   expect(instance.value).toBe('foo')
+  // })
+
+  // it('should support default export of anonymous generator function', () => {
+  //   const interpreter = new Sval({ sourceType: 'module' })
+
+  //   interpreter.run(`
+  //     export default function*() {
+  //       yield 'foo'
+  //     }
+  //   `)
+
+  //   const generator = interpreter.exports.default()
+  //   expect(generator.next().value).toBe('foo')
+  // })
+
 })
